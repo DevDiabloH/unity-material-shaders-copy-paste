19f9af7dfa386867d23d1d6d04fc3c18de6043e1
DevDiabloH <50089624+DevDiabloH@users.noreply.github.com>
2021-02-10 12:58:40 +0900

    Update MaterialParamCopyAndPaste.cs

--- Assets/Editor/MaterialParamCopyAndPaste.cs
+++ Assets/Editor/MaterialParamCopyAndPaste.cs
@@ -19,20 +19,52 @@ public class MaterialParamCopyAndPaste : EditorWindow
         GUILayout.Label("Tool", EditorStyles.boldLabel);
         masterMaterial = EditorGUILayout.ObjectField("MasterMaterial", masterMaterial, typeof(Material), true) as Material;
 
-        if (GUILayout.Button("Run Paste"))
+        if (null == masterMaterial)
         {
-            if(masterMaterial != null)
+            if(GUILayout.Button("Set Master Material", GUILayout.Height(40)))
             {
-                Paste();
+                PushMasterMaterial();
             }
-            else
+        }
+        else
+        {
+            if (GUILayout.Button("Run Paste", GUILayout.Height(40)))
             {
-                Debug.LogWarning("MasterMaterial is null.");
+                Apply();
+            }
+
+            if (GUILayout.Button("Clear Material", GUILayout.Height(40)))
+            {
+                ClearMaterial();
             }
         }
     }
 
-    private void Paste()
+    private void PushMasterMaterial()
+    {
+        if (Selection.activeGameObject)
+        {
+            masterMaterial = GetMaterialByGameOjbect();
+        }
+        else if (Selection.GetFiltered(typeof(Material), SelectionMode.Assets).Length == 1)
+        {
+            masterMaterial = GetMaterialByProjectFolder();
+        }
+    }
+
+    private void Apply()
+    {
+        if (Selection.gameObjects.Length != 0)
+        {
+            SetPasteInGameObject();
+        }
+        else if (Selection.GetFiltered(typeof(Material), SelectionMode.Assets).Length != 0)
+        {
+            SetPasteInProjectFolderSelection();
+        }
+    }
+
+    private void SetPasteInProjectFolderSelection()
     {
         foreach(Material mt in Selection.GetFiltered(typeof(Material), SelectionMode.Assets))
         {
@@ -41,5 +73,51 @@ public class MaterialParamCopyAndPaste : EditorWindow
         }
 
         Debug.Log("Paste Complete");
+        ClearMaterial();
+    }
+
+    private void SetPasteInGameObject()
+    {
+        foreach(GameObject obj in Selection.gameObjects)
+        {
+            if (obj.GetComponent<MeshRenderer>())
+            {
+                foreach(Material mt in obj.GetComponent<MeshRenderer>().sharedMaterials)
+                {
+                    mt.shader = masterMaterial.shader;
+                    mt.CopyPropertiesFromMaterial(masterMaterial);
+                }
+            }
+        }
+
+        Debug.Log("Paste Complete");
+        ClearMaterial();
+    }
+
+    private Material GetMaterialByGameOjbect()
+    {
+        if (null != Selection.activeGameObject.GetComponent<MeshRenderer>())
+        {
+            return Selection.activeGameObject.GetComponent<MeshRenderer>().sharedMaterials[0];
+        }
+
+        Debug.LogWarning("MeshRenderer Component is not found. -> " + Selection.activeGameObject.name);
+        return null;
+    }
+
+    private Material GetMaterialByProjectFolder()
+    {
+        if(Selection.GetFiltered(typeof(Material), SelectionMode.Assets).Length == 1)
+        {
+            return Selection.GetFiltered(typeof(Material), SelectionMode.Assets)[0] as Material;
+        }
+
+        Debug.LogWarning("On selection material is not found.");
+        return null;
+    }
+
+    private void ClearMaterial()
+    {
+        masterMaterial = null;
     }
 }
