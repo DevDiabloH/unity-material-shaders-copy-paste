839b6d25f2ab5981eb95ea9e023910be9509b9da
DevDiabloH <50089624+DevDiabloH@users.noreply.github.com>
2021-02-10 10:16:30 +0900

    add
    shader copy and paste tool

---
+++ Assets/Editor/MaterialParamCopyAndPaste.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MaterialParamCopyAndPaste : EditorWindow
+{
+    private Material masterMaterial = null;
+
+    [MenuItem("CustomTools/MaterialParamCopyAndPaste")]
+    static void CopyAndPaste()
+    {
+        MaterialParamCopyAndPaste window = (MaterialParamCopyAndPaste)EditorWindow.GetWindow(typeof(MaterialParamCopyAndPaste));
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Tool", EditorStyles.boldLabel);
+        masterMaterial = EditorGUILayout.ObjectField("MasterMaterial", masterMaterial, typeof(Material), true) as Material;
+
+        if (GUILayout.Button("Run Paste"))
+        {
+            if(masterMaterial != null)
+            {
+                Paste();
+            }
+            else
+            {
+                Debug.LogWarning("MasterMaterial is null.");
+            }
+        }
+    }
+
+    private void Paste()
+    {
+        foreach(Material mt in Selection.GetFiltered(typeof(Material), SelectionMode.Assets))
+        {
+            mt.shader = masterMaterial.shader;
+            mt.CopyPropertiesFromMaterial(masterMaterial);
+        }
+
+        Debug.Log("Paste Complete");
+    }
+}
